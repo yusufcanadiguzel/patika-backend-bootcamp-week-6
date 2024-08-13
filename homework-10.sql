0537d36b89c16a90e293430207276e19edfa198b
Yusufcan Adıgüzel <yusufcanadgzl@outlook.com>
2024-08-13 14:30:11 +0300

    Homework 9-10-11 Completed

---
+++ homework-10.sql
@@ -0,0 +1,18 @@
+-- ODEV 10 --
+
+-- city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz LEFT JOIN sorgusunu yazınız.
+SELECT country.country, city.city  FROM country
+LEFT JOIN city
+ON country.country_id = city.country_id;
+
+-- customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz 
+-- RIGHT JOIN sorgusunu yazınız.
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM payment
+RIGHT JOIN customer
+ON payment.customer_id = customer.customer_id;
+
+-- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz 
+-- FULL JOIN sorgusunu yazınız.
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM customer
+FULL JOIN rental
+ON customer.customer_id = rental.customer_id;
